d172c76072d9ed1f8af5b9d1e5acbf96ad0de0a7
Sreehari Pulickamadhom Sreedhar <orectique@gmail.com>
2021-09-06 09:25:00 +0530

    Update Numerical.py

--- src/Sem3Py/Numerical.py
+++ src/Sem3Py/Numerical.py
@@ -34,24 +34,27 @@ class roots:
         a = lbound
         b = ubound
         itn = 0
-        while itn <= max_itn and abs(a-b) >= delta:
-            x = (a + b)/2
-            if itn == 0:
-                arr = [a, b, x, self.func(x)]
-            else:
-                arr = np.vstack([arr, [a, b, x, self.func(x)]])
-            if self.func(x) > 0:
-                b = x
-            elif self.func(x) < 0:
-                a = x
-            else:
-                return arr
-            itn += 1
-
-        return arr
-
         
+        if self.func(a)*self.func(b) > 0:
+            return 'Invalid upper and lower limits'
+        
+        else:
+            while itn <= max_itn and abs(a-b) >= delta:
+                x = (a + b)/2
+                if itn == 0:
+                    arr = [a, b, x, self.func(x)]
+                else:
+                    arr = np.vstack([arr, [a, b, x, self.func(x)]])
+                if self.func(x) > 0:
+                    b = x
+                elif self.func(x) < 0:
+                    a = x
+                else:
+                    return arr
+                itn += 1
 
+            return arr
+        
     def incremental():
         return
     
